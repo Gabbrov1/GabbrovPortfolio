8d786a400835bf64f49ac37945f748121dc8ae65
Gabbrov1 <ionut4965@gmail.com>
2026-05-09 05:28:50 +0100

    added defer to redirect website

--- Scripts/redirect.js
+++ Scripts/redirect.js
@@ -1,1 +1,12 @@
 window.location.href = './Pages/home.html';
+
+
+
+function redirectToHome(timer) {
+    setTimeout(() => {
+        window.location.href = './Pages/home.html';
+    }, timer);
+    redirectToHome(timer);
+}
+
+redirectToHome(1000);
